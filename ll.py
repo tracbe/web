f7726741159dce2c92e99bc59ca83cc6f0637c85
tracbe <norhanbadran499@gmail.com>
2025-08-26 16:39:14 +0300

    Update ll.py

--- ll.py
+++ ll.py
@@ -3,27 +3,28 @@ import streamlit as st
 
 st.title("sidebar less")
 
-st.sidebar.image("c:/Users/Pc SToRe/Downloads/download.jpeg")
+st.sidebar.image("download.jpeg")
 
 age = st.sidebar.slider("age",0,100)
 st.sidebar.selectbox("select:", [1 , 2 ,3])
 
 c1 ,c2 ,c3 = st.sidebar.columns(3)
 with c1 :
-    st.image("c:/Users/Pc SToRe/Downloads/download.jpeg")
+    st.image("download.jpeg")
 with c2 :
-    st.image("c:/Users/Pc SToRe/Downloads/images.jpeg")
+    st.image("download.jpeg")
 with c3 :
-    st.image("c:/Users/Pc SToRe/Downloads/download.jpeg")
+    st.image("download.jpeg")
 
 x1 , x2 ,x3, x4 , x5 = st.columns(5)
 with x1 :
-     st.image("c:/Users/Pc SToRe/Downloads/download.jpeg")
+     st.image("download.jpeg")
 
 
 word = "link"
 link = "https://www.w3schools.com/python/python_getstarted.asp"
 
 st.markdown(f"press [{word}]({link})")
+
 
 st.markdown(f"press [LINK]('https://www.w3schools.com/python/python_getstarted.asp')")
